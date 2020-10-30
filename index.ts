9a16ff1cabeb745deac1ccb9c2c6bb861853e5fe
dennisko <d.kortsch@searchmetrics.com>
2020-10-30 08:43:33 +0100

    add node-fetch

--- index.ts
+++ index.ts
@@ -1,8 +1,16 @@
 import express from "express";
+import fetch from "node-fetch";
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
+app.get("/hello", (req, res) => {
+  res.json("Hello World");
+});
+
+app.get("/test", async (req, res) => {
+  const response = await fetch("http://localhost:3000/hello");
+  const test = await response.json();
+  res.send(test);
 });
 
 const PORT = process.env.PORT || 3000;
